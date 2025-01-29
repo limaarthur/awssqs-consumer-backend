01761ceac6e91f90aeb74ce91ec816ee563c512a
limaarthur <artglima.dev@gmail.com>
2025-01-29 15:17:05 -0300

    feat: first commit

---
+++ src/main/java/com/ignitec/awssqsconfiguration/AwssqsconfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.ignitec.awssqsconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwssqsconfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwssqsconfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ignitec/awssqsconfiguration/AwssqsconfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignitec.awssqsconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwssqsconfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
